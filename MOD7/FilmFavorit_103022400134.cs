396596dbac558f708f5a0818b370579046419db1
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-15 07:42:22 +0700

    menambahkan class Ridho

---
+++ MOD7/FilmFavorit_103022400134.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOD7
+{
+    internal class FilmFavorit_103022400134
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+    }
+}
--- MOD7/Program.cs
+++ MOD7/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Text.Json;
+using MOD7;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        ReadJSON();
+    }
+
+    static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("jurnal7_1_103022400134.json");
+        var data = JsonSerializer.Deserialize<FilmFavorit_103022400134>(jsonString);
+
+
+        Console.WriteLine($"Title: {data.title}");
+        Console.WriteLine($"Director: {data.director}");
+        Console.WriteLine($"Year: {data.year}");
+        Console.WriteLine($"Rating: {data.rating}");
+        Console.WriteLine($"Duration: {data.durationMinutes} minutes");
+        Console.WriteLine($"Watched: {data.isWatched}");
+    }
+}
