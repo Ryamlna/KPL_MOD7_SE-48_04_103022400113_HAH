d893f51e88e7246dd5294ccdc69932ec0edd3f10
miaw81331 <miaw81331@gmail.com>
2026-04-15 07:46:18 +0700

    Menambahkan Class FilmFavorit dan Program.cs

---
+++ MOD7/FilmFavorit_103022400113.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class FilmFavorit_103022400113
+{
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public int rating { get; set; }
+    public int durationMinutes { get; set; }
+    public bool isWatched { get; set; }
+}
--- MOD7/Program.cs
+++ MOD7/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadJSON1();
+        Console.WriteLine();
+    }
+
+    private static void ReadJSON1()
+    {
+        string jsonString = File.ReadAllText("jurnal7_1_103022400113.json");
+        var data = JsonSerializer.Deserialize<FilmFavorit_103022400113>(jsonString);
+        Console.WriteLine($"Title : {data.title}");
+        Console.WriteLine($"Director : {data.director}");
+        Console.WriteLine($"Year : {data.year}");
+        Console.WriteLine($"Genre : {data.genre}");
+        Console.WriteLine($"Rating : {data.rating}");
+        Console.WriteLine($"Duration : {data.durationMinutes}");
+        Console.WriteLine($"Watched : {data.isWatched}");
+
+    }
+}
